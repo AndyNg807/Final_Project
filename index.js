f2781fd7961e6700895a737160982a3ed6ce46e3
andyng <chungn807@gmail.com>
2020-04-06 18:01:43 -0700

    sort out index.js

--- index.js
+++ index.js
@@ -23,20 +23,7 @@ const Product = require('./modules/product');
 const deleteImage = require('./modules/deleteImage');
 const upload = require('./modules/uploadImage');
 
-/*
-//This is what you use to have multipart form data
-const multer = require('multer');
-// SET STORAGE
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'public/img/product')
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now() + '-' + file.originalname)
-    }
-})
-const upload = multer({ storage: storage });
-*/
+
 //set global variable
 let login = false;
 let admin = false;
